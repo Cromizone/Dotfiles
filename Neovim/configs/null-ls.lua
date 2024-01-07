5575ed001c55aee7c53522e665d6877cdcb54fd9
Cromizone <cromizonebolt@gmail.com>
2024-01-07 09:28:41 +0530

    Updated mypy and black configuration

--- Neovim/configs/null-ls.lua
+++ Neovim/configs/null-ls.lua
@@ -4,8 +4,14 @@ local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
 local sources = {
-   formatting.black,
-   lint.mypy,
+   formatting.black.with({
+    extra_args = {"-l", "100"},
+  }),
+
+   lint.mypy.with({
+      extra_args = {"--ignore-missing-imports", "--python-executable", "/usr/bin/python"},
+      method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+  }),
 }
 
 null_ls.setup {
